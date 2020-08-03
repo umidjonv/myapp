8324b8569f278998473faa56476fb9c753ad712f
makhmudovi <isfan2006@mail.ru>
2020-08-03 14:51:12 +0500

    change product model

---
+++ productsapi/Repositories/Iproduct.cs
@@ -0,0 +1,15 @@
+﻿using productsapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productsapi.Repositories
+{
+    interface Iproduct
+    {
+        List<Product> getAll();
+        Product gaetOneById();
+        Guid addNew(Product product);
+    }
+}
---
+++ productsapi/Repositories/ProductRepo.cs
@@ -0,0 +1,27 @@
+﻿using productsapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productsapi.Repositories
+{
+    public class ProductRepo : Iproduct
+    {
+        public List<Product> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product gaetOneById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid addNew(Product product)
+        {
+            throw new NotImplementedException();
+        }
+     
+    }
+}
